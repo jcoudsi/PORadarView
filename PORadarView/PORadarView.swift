e9e1b2303f5447d2024f66949782290f7b84cbdf
Julien Coudsi <julien.coudsi@backelite.com>
2016-08-18 13:35:48 +0200

    Rewrite rotation animation + refactoring

--- PORadarView/PORadarView.swift
+++ PORadarView/PORadarView.swift
@@ -8,208 +8,225 @@
 
 import UIKit
 
-let π:CGFloat = CGFloat(M_PI)
+let π: CGFloat = CGFloat(M_PI)
 
 @IBDesignable public class PORadarView: UIView {
-    
-    private var lineCircleStartAngleInDegrees : CGFloat = 0
-    private var lineCircleWidthInDegrees : CGFloat = 30
-    private var radius:CGFloat = 0
-    private var rectCenter:CGPoint = CGPoint(x: 0,y: 0)
-    private var imageView:UIImageView?
-    private var detectionItemShapeLayer:CAShapeLayer?
-    
+
+    private var lineCircleStartAngleInDegrees: CGFloat = 0
+    private var lineCircleWidthInDegrees: CGFloat = 30
+    private var radius: CGFloat = 0
+    private var rectCenter: CGPoint!
+    private var imageView: UIImageView?
+    private var detectionItemShapeLayer: CAShapeLayer!
+
     public var isImageDisplayed = false
     public var isDetecting = false
 
-    public var detectionItemColor:UIColor = UIColor.blueColor()
-    public var contentCircleColor:UIColor = UIColor.greenColor()
-    
+    public var detectionItemColor: UIColor = UIColor.blueColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+
+    public var contentCircleColor: UIColor = UIColor.greenColor() {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+
     public required init?(coder aDecoder: NSCoder) {
-        
+
         super.init(coder: aDecoder)
     }
-    
+
     override public required init(frame: CGRect) {
-        
+
         super.init(frame: frame)
-        
+
         self.backgroundColor = UIColor.clearColor()
     }
-    
+
+    //#MARK: - Drawing
+
     override public func drawRect(rect: CGRect) {
         self.drawContentCicle(rect)
     }
-    
+
+
     private func drawContentCicle(rect: CGRect) {
-        
+
         self.rectCenter = CGPoint(x: CGRectGetMidX(rect), y: CGRectGetMidY(rect))
         self.radius = rect.size.width/2
-        
+
         let contentCirclePath = UIBezierPath(arcCenter: self.rectCenter,
             radius: self.radius,
             startAngle: 0.0,
             endAngle: 2*π,
             clockwise: true)
-        
+
         self.contentCircleColor.setFill()
         contentCirclePath.fill()
     }
-    
-    private func createDetectionItemPathAtStartDegreesAngle(startDegreesAngle:CGFloat, endDegreesAngle:CGFloat) -> UIBezierPath {
 
-        let detectionItemPath = UIBezierPath(arcCenter: self.rectCenter,
+    private func createDetectionItemPathAtStartDegreesAngle(startDegreesAngle: CGFloat, endDegreesAngle: CGFloat) -> UIBezierPath {
+
+        let detectionItemPath = UIBezierPath(arcCenter: CGPoint(x: self.rectCenter.x, y:self.rectCenter.y),
             radius: self.radius,
             startAngle: convertToRadians(startDegreesAngle),
             endAngle: convertToRadians(startDegreesAngle) + convertToRadians(endDegreesAngle),
             clockwise: true)
-        
+
         detectionItemPath.addLineToPoint(self.rectCenter)
         detectionItemPath.closePath()
-        
+
         return detectionItemPath
     }
-    
-    
-    public func setImage(image:UIImage) {
-        
-        guard let imageView:UIImageView = UIImageView(image: image) else {
+
+
+    private func createDetectionItemShapeLayer() -> CAShapeLayer {
+
+        let detectionItemLayer = CAShapeLayer()
+        detectionItemLayer.path = self.createDetectionItemPathAtStartDegreesAngle(0, endDegreesAngle: 30).CGPath
+        detectionItemLayer.frame = CGPathGetBoundingBox(detectionItemLayer.path)
+        detectionItemLayer.bounds = CGPathGetBoundingBox(detectionItemLayer.path)
+        detectionItemLayer.fillColor = detectionItemColor.CGColor
+        detectionItemLayer.anchorPoint = CGPoint(x: 0, y: 0)
+        detectionItemLayer.position = self.rectCenter
+
+        return detectionItemLayer
+
+    }
+
+    public func setImage(image: UIImage) {
+
+        guard let imageView: UIImageView = UIImageView(image: image) else {
             return
         }
-        
+
         let frameWidth = self.frame.size.width
         let frameHeight = self.frame.size.height
         let imageWidth =  frameWidth/2
         let imageHeight = frameHeight/2
-        
+
         imageView.frame = CGRectMake(frameWidth / 2 - imageWidth/2, frameHeight / 2 - imageHeight/2, imageWidth, imageHeight)
         self.imageView = imageView
 
     }
-    
+
+    //#MARK: - Animation
+
     public func startAnimateDetection() {
-        
-        if (detectionItemShapeLayer == nil) {
-            
-            detectionItemShapeLayer = CAShapeLayer()
-            detectionItemShapeLayer?.fillColor = detectionItemColor.CGColor
-            self.layer.addSublayer(detectionItemShapeLayer!)
+
+        if (self.detectionItemShapeLayer == nil) {
+            self.detectionItemShapeLayer = self.createDetectionItemShapeLayer()
         }
-        
-        animate()
-    }
-    
-    public func animate() {
-        
-        let anim:CABasicAnimation = CABasicAnimation(keyPath: "path")
-        anim.duration = 0.1
-        anim.fromValue = self.createDetectionItemPathAtStartDegreesAngle(self.lineCircleStartAngleInDegrees, endDegreesAngle: self.lineCircleWidthInDegrees).CGPath
-        
-        if (self.lineCircleStartAngleInDegrees+10 >= 360) {
-            self.lineCircleStartAngleInDegrees = 0
-        } else {
-            self.lineCircleStartAngleInDegrees+=10
-        }
-        
-        anim.toValue = self.createDetectionItemPathAtStartDegreesAngle(self.lineCircleStartAngleInDegrees, endDegreesAngle: self.lineCircleWidthInDegrees).CGPath
-        anim.delegate = self;
-        anim.removedOnCompletion = false;
-        anim.fillMode = kCAFillModeForwards;
-        
-        detectionItemShapeLayer!.addAnimation(anim, forKey: "animateRadar")
-    }
-    
-    override public func animationDidStop(anim: CAAnimation, finished flag: Bool) {
-        
-        if (self.isDetecting) {
-            animate()
-        }
+
+        self.layer.addSublayer(self.detectionItemShapeLayer)
+        self.animate()
     }
 
+    public func animate() {
+
+        let animateRotation = CABasicAnimation(keyPath: "transform.rotation")
+        animateRotation.toValue = M_PI*2
+        animateRotation.additive = true
+        animateRotation.repeatCount = Float.infinity
+        animateRotation.duration = 5
+
+        self.detectionItemShapeLayer.addAnimation(animateRotation, forKey: "animateRadar")
+    }
+
+    //#MARK: - Detection management
+
     public func startDetection() {
-        
-        if (!self.isDetecting) {
-            startAnimateDetection()
-            self.isDetecting = true
+
+        guard !self.isDetecting else {
+            return
         }
-        
-        self.setNeedsDisplay()
+
+        self.startAnimateDetection()
+        self.isDetecting = true
+
     }
-    
+
     public func stopDetection() {
-        
-        if (self.isDetecting) {
-            self.isDetecting = false
-            self.detectionItemColor = self.contentCircleColor
-            self.detectionItemShapeLayer?.removeAllAnimations()
+
+        guard self.isDetecting else {
+            return
         }
-        self.setNeedsDisplay()
+
+        self.isDetecting = false
+        self.detectionItemColor = self.contentCircleColor
+        self.detectionItemShapeLayer?.removeAllAnimations()
+        self.detectionItemShapeLayer.removeFromSuperlayer()
     }
-    
-    
-    public func objectDetected() {
-        
-        if (!self.isImageDisplayed) {
-        
-            self.addSubview(self.imageView!)
-            self.showImageWithAnimation()
-            self.isImageDisplayed = true
-        }
-    }
-    
+
     public func objectDetectedWithAutoStopDetection() {
         self.objectDetected()
         self.stopDetection()
     }
-    
+
     public func objectUndetectedWithAutoStopDetection() {
         self.objectUndetected()
         self.stopDetection()
     }
-    
+
     public func objectUndetectedWithAutoRestartDetection() {
         self.objectUndetected()
         self.startDetection()
     }
-    
+
+    public func objectDetected() {
+
+        guard !self.isImageDisplayed, let imageView = self.imageView else {
+            return
+        }
+
+        self.addSubview(imageView)
+        self.showImageWithAnimation(imageView)
+        self.isImageDisplayed = true
+
+    }
+
     public func objectUndetected() {
         self.hideImageWithAnimation()
+        self.isImageDisplayed = false
     }
-    
-    private func showImageWithAnimation() {
-        
-        weak var weakSelf = self
-        
+
+
+    //#MARK: - Image
+
+    private func showImageWithAnimation(imageView: UIImageView) {
+
         UIView.animateWithDuration(0.3, animations: { () -> Void in
-            weakSelf!.imageView!.transform = CGAffineTransformMakeScale(1.2, 1.2)
-            
+            imageView.transform = CGAffineTransformMakeScale(1.2, 1.2)
+
             }, completion: { (Bool) -> Void in
-                
+
                 UIView.animateWithDuration(0.4, animations: { () -> Void in
-                    weakSelf!.imageView!.transform = CGAffineTransformMakeScale(0.8, 0.8)
+                    imageView.transform = CGAffineTransformMakeScale(0.8, 0.8)
                 })
         })
     }
-    
+
     private func hideImageWithAnimation() {
-        
-        weak var weakSelf = self
-        
+
+        guard let imageView = self.imageView else {
+            return
+        }
+
         UIView.animateWithDuration(0.3, animations: { () -> Void in
-            weakSelf!.imageView?.transform = CGAffineTransformMakeScale(0.1, 0.1)
-            
+            imageView.transform = CGAffineTransformMakeScale(0.1, 0.1)
+
             }, completion: { (Bool) -> Void in
-                weakSelf!.imageView?.removeFromSuperview()
-                weakSelf!.isImageDisplayed = false
+                imageView.removeFromSuperview()
         })
     }
-    
-    private func convertToRadians(degrees:CGFloat) -> CGFloat  {
+
+    //#MARK: - Helpers
+
+    private func convertToRadians(degrees: CGFloat) -> CGFloat {
         return (degrees*π)/180
     }
-    
-    
-    
-    
-    
+
 }
--- PORadarViewExample/PORadarViewExample/MainViewController.swift
+++ PORadarViewExample/PORadarViewExample/MainViewController.swift
@@ -10,51 +10,53 @@ import UIKit
 import PORadarView
 
 class MainViewController: UIViewController {
-    
-    var isAnimating:Bool = false
-    var displayFromPairing:Bool = false
-    var radarView:PORadarView!
-    
+
+    var isAnimating: Bool = false
+    var displayFromPairing: Bool = false
+    var radarView: PORadarView!
+
     @IBOutlet weak var radarContainerView: UIView!
-    
+
     override func viewDidLoad() {
-        
+
         super.viewDidLoad()
-        
+
         self.radarView = PORadarView(frame: self.radarContainerView.bounds)
-        
-        self.radarContainerView.addSubview(self.radarView)
-        
         self.radarView.setImage(UIImage(named:"new-item-example")!)
-        
-        self.configureForExample1(self)
+
+        self.radarContainerView.addSubview(self.radarView)
 
     }
-    
-    
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        self.configureForExample1(self)
+    }
+
+
     @IBAction func configureForExample1(sender: AnyObject) {
-        
+
         self.radarView.contentCircleColor = ColorHelper.getLightRedColor()
         self.radarView.detectionItemColor = ColorHelper.getMediumRedColor()
-        
+
         self.radarView.objectUndetectedWithAutoRestartDetection()
     }
-    
+
     @IBAction func configureForExample2(sender: AnyObject) {
-        
+
         self.radarView.contentCircleColor = ColorHelper.getMediumOrangeColor()
         self.radarView.detectionItemColor = ColorHelper.getMediumRedColor()
-        
+
         self.radarView.objectDetected()
         self.radarView.startDetection()
     }
-    
+
     @IBAction func configureForExample3(sender: AnyObject) {
-        
+
         self.radarView.contentCircleColor = ColorHelper.getMediumGreenColor()
-        
+
         self.radarView.objectDetectedWithAutoStopDetection()
     }
-    
-}
 
+}
